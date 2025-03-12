e0d1fa84f66816d65775a6dc656d2fe595bd271c
BIRGITA_ANASTASYA <birgita.hutabarat@gmail.com>
2025-03-12 13:50:05 +0700

    Menambahkan HomeController dan halaman kontak serta memperbarui routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//      return view('welcome');
+//});
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/contact', [HomeController::class, 'contact']);
+
